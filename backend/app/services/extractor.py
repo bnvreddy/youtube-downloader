93a0605bc19aa9a22221cb7b79cd966dff46660d
bnvreddy <bnvreddy92@gmail.com>
2026-06-09 13:27:22 +0530

    Changed concurrency settings

--- backend/app/services/extractor.py
+++ backend/app/services/extractor.py
@@ -72,7 +72,9 @@ def get_video_info(url: str):
         # 'geo_bypass_country': 'US',
         'cookiefile': 'youtube_cookies.txt',
         # 'extractor_args': {'youtube': {'player_client': ['android']}},
-        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863'
+        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863',
+        'sleep_interval' : 3,
+        'concurrent_fragment_downloads': 1
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -106,7 +108,9 @@ def list_available_formats(url: str):
         # 'geo_bypass_country': 'US',
         'cookiefile': 'youtube_cookies.txt',
         # 'extractor_args': {'youtube': {'player_client': ['android']}},
-        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863'
+        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863',
+        'sleep_interval' : 3,
+        'concurrent_fragment_downloads': 1
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -181,7 +185,9 @@ def list_available_subtitles(url: str):
         # 'geo_bypass_country': 'US',
         'cookiefile': 'youtube_cookies.txt',
         # 'extractor_args': {'youtube': {'player_client': ['android']}},
-        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863'
+        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863',
+        'sleep_interval' : 3,
+        'concurrent_fragment_downloads': 1
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -260,7 +266,9 @@ def download_video_to_temp(url: str, task_id: str, max_resolution: int = 720, au
         # 'geo_bypass_country': 'US',
         'cookiefile': 'youtube_cookies.txt',
         # 'extractor_args': {'youtube': {'player_client': ['android']}},
-        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863'
+        'proxy': 'http://bywzztyc:rkafyc1ko1ds@38.154.203.95:5863',
+        'sleep_interval' : 3,
+        'concurrent_fragment_downloads': 1
     }
     
     if merge_format: 
